bea513b0909a821cc5156206125153c25eae7831
PiwoZz <ocuspocuslotus@hotmail.com>
2025-09-30 18:47:55 +0200

    Added some base architecture

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jump;
+    [SerializeField] private float fall;
+    private bool _canJump;
+
+    private void Update()
+    {
+        Vector2 direction = InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
+        if (direction != Vector2.zero)
+        {
+            
+        }
+    }
+}
